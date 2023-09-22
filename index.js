a0b697ab06f0de11c3205bedc27c5982341d3f7f
AlfG71 <56656187+AlfG71@users.noreply.github.com>
2023-09-22 12:57:44 -0700

    Completed lab

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
+
 // Import of the data from './data.json'
 const data = require('./data');
 
@@ -16,8 +17,26 @@ mongoose
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    // Recipe.create(data);  Iteration 2
+    Recipe.insertMany(data) // Iteration 3
+    data.forEach(recipe => console.log(recipe.title))
+    return Recipe.insertMany(data)
+  })
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, { duration: 100 }) // Iteration 4, findOneAndUpdate(arg1 = condition, arg2 = updated)
+  })
+  .then(() => {
+    Recipe.deleteOne({title: "Carrot Cake"})//{title: "Carrot Cake"})//Iteration 5
+    console.log("A success message")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
+;
+
+//Close the connection
+mongoose.connection.close(); //Iteration 6
+
+
+
+
